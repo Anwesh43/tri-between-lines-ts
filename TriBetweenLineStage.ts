c5d855ede0fcb00671cb7643c046554833080f89
Anwesh43 <anweshthecool0@gmail.com>
2019-01-12 22:05:08 +0530

    adding functions

--- TriBetweenLineStage.ts
+++ TriBetweenLineStage.ts
@@ -6,3 +6,22 @@ const scGap : number = 0.05
 const scDiv : number = 0.51
 const strokeFactor : number = 90
 const sizeFactor : number = 3
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(0, maxScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) => Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateScale : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
