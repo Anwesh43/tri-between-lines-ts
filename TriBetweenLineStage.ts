c5dc6e64a6fa76e0b2ffc7935b5c077b432b0fe7
Anwesh43 <anweshthecool0@gmail.com>
2019-01-12 21:54:42 +0530

    adding constants

---
+++ TriBetweenLineStage.ts
@@ -0,0 +1,8 @@
+const nodes : number = 5
+const lines : number = 2
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 3
