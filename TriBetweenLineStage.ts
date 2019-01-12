1a09088151bc596ba7816314059c57d3eabd9b69
Anwesh43 <anweshthecool0@gmail.com>
2019-01-12 22:57:30 +0530

    adding State class

--- TriBetweenLineStage.ts
+++ TriBetweenLineStage.ts
@@ -45,14 +45,16 @@ const drawTBLNode : Function = (context : CanvasRenderingContext2D, i : number, 
     const xGap : number = (2 * size) / lines
     context.save()
     context.translate(gap * (i + 1), h/2)
+    context.rotate(Math.PI/2 * sc2)
     drawParallelLines(context, size/2)
     for (var j = 0; j < lines; j++) {
+        const sc = divideScale(sc1, j, lines)
         const sf = 1 - 2 * (j % 2)
         context.save()
         context.translate(-size + j * xGap, sf * size)
         context.beginPath()
         context.moveTo(0, 0)
-        context.lineTo(xGap, -sf * size)
+        context.lineTo(xGap * sc, -sf * size * sc)
         context.stroke()
         context.restore()
     }
@@ -86,5 +88,28 @@ class TriBetweenLineStage {
         stage.initCanvas()
         stage.render()
         stage.handleTap()
+    }
+}
+
+class State {
+    scale : number = 0
+    prevScale : number = 0
+    dir : number = 0
+
+    update(cb : Function) {
+        this.scale += updateScale(this.scale, this.dir, lines, 1)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
     }
 }
